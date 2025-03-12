f141ebf2c71d3aad17c461d4e7397f16f8ff216d
EmanuelIna <emanuelinacio052@gmail.com>
2025-03-11 21:17:15 -0400

    Basic H2 implemented (With Error)

--- src/main/java/me/dio/domain/model/Feature.java
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -1,31 +1,9 @@
 package me.dio.domain.model;
 
-public class Feature {
+import jakarta.persistence.Entity;
 
-    private Long id;
-    private String icon;
-    private String description;
+@Entity(name = "tb_features")
+public class Feature extends BaseItem{
 
-    //#region getters-setter
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getIcon() {
-        return icon;
-    }
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    //#endregion
 
 }
